e35ff05599a0602d3f1649f352f4d4c6ff0cfa47
Sand Man <rajeshbhadra62@gmail.com>
2023-01-09 23:45:16 +0530

    first commit

---
+++ TakeInput.py
@@ -0,0 +1,3 @@
+# for scanning value from user your need to use "input" term
+sc = input("Enter your value: ");
+print(sc);
---
+++ program1.py
@@ -0,0 +1,2 @@
+#To print in display you need to use "print" term
+print("This is only for testing is the enviroment ready for python");
