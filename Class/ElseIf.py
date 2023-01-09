d80fa7936d7cb84a3c1575e676375b83db527438
Sand Man <rajeshbhadra62@gmail.com>
2023-01-10 00:22:15 +0530

    else if condition

---
+++ Class/ElseIf.py
@@ -0,0 +1,10 @@
+# From this code we will learn how to use else if condition
+a = 10
+b = 20
+c = 30
+if a > b > c:
+    print("a is largest value")
+elif b > a > c:
+    print("b is largest value")
+else:
+    print("c is largest value")
