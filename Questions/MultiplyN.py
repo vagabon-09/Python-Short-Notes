d1bbe93e9b0b9a7f30ba26295806477a17b7372a
Sand Man <rajeshbhadra62@gmail.com>
2023-01-10 15:58:33 +0530

    lab- 10/1/23

---
+++ Questions/MultiplyN.py
@@ -0,0 +1,3 @@
+#In this program we will see how to multiply like n+nn+nnn
+n=int(input("Enter the value of n: "))
+print(n+(n*n)+(n*n*n))
---
+++ Questions/PrintFirstLast.py
@@ -0,0 +1,3 @@
+# In this program we will see how to print first and last element from a list
+color_list = ["Red","Orange","Green","Black"]
+print(color_list[0]+" and "+color_list[len(color_list)-1])
