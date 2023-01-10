012f35fd64e6713be82d2c858d3db20d1abc8b77
Sand Man <rajeshbhadra62@gmail.com>
2023-01-10 15:47:54 +0530

    lab 10/1/23

---
+++ Questions/CircleRadius.py
@@ -0,0 +1,4 @@
+#Write a Python progrma which accepts the radius of a circle from the user and compute the area
+
+r = int(input("Enter the radius of the circle: "))
+print("Area of the circle is "+ str(3.14*(r**2))) # "r**2" mean power of r is 2
---
+++ Questions/ReverseString.py
@@ -0,0 +1,5 @@
+# In this program we will see how to reverse string
+fName = input("Enter your first name: ")
+lName = input("Enter your last name: ")
+name = fName + " " + lName
+print(name[::-1])
