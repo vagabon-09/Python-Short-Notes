3f0ae951ef5087dec22454df45e390ab69966d6d
Sand Man <rajeshbhadra62@gmail.com>
2023-01-18 00:05:45 +0530

    updating Questions

--- Questions/CountDublicate.py
+++ Questions/CountDublicate.py
@@ -5,3 +5,6 @@ for i in range(0, len(arr)):
     if arr[i] == 4:
         count +=1
 print(count)
+
+# Python shortcut count function to find dublicate variable in a given array
+print(arr.count(4))
