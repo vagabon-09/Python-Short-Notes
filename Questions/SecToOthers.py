e6ad0e97c3638bb9972c7806935e8b28a87471e7
Sand Man <rajeshbhadra62@gmail.com>
2023-01-31 16:13:56 +0530

    new question added

--- Questions/SecToOthers.py
+++ Questions/SecToOthers.py
@@ -1,8 +1,13 @@
 # 29.	 Write a Python program to convert seconds to day, hour, minutes, and seconds.
-sec = int(input("Enter sec"))
-day = sec//(3600*24)
-min = sec//60
-hour = sec//3600
-print(day)
-print(min)
-print(hour)
+sec = int(input("Enter sec: "))
+if sec >= 60:
+    min = sec//60
+    print(min)
+elif sec >= 3600:
+    hour = sec//3600
+    print(hour)
+elif sec >= 3600*24:
+    day = sec//(3600*24)
+    print(day)
+else:
+    print(sec)
