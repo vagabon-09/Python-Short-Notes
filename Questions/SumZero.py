bbb60dbc06e87ee4ed4f457f6836030612981034
Sand Man <rajeshbhadra62@gmail.com>
2023-01-31 15:03:53 +0530

    new question is added

---
+++ Questions/SumZero.py
@@ -0,0 +1,8 @@
+# Write a Python program to sum of three given integers. However, if two values are equal sum will be zero.
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+sum = 0
+if a == b:
+    print(sum)
+else:
+    print(a+b)
