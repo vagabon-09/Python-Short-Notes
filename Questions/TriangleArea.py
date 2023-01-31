fdd38d2896d49abfd44f35af82db094a041256a5
Sand Man <rajeshbhadra62@gmail.com>
2023-01-31 14:54:02 +0530

    trianlge area calculate question is added

---
+++ Questions/TriangleArea.py
@@ -0,0 +1,6 @@
+# 22.	 Write a Python program that will accept the base and height of a triangle and compute the area.
+print("Program to calculate Triangle area: \n ")
+b = int(input("Enter baes: "))
+h = int(input("Enter height: "))
+are = 1/2*(b*h)
+print("area is " + are)
