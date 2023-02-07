2dad6706636d0859f18953970529c48156c570f8
Sand Man <rajeshbhadra62@gmail.com>
2023-02-07 15:30:20 +0530

    new question is added

---
+++ Questions/AsciiValue.py
@@ -0,0 +1,4 @@
+# 36.	 Write a Python program to get the ASCII value of a character.
+n = input("Enter a charectre: ")
+print("Ascii value of your input is :")
+print(ord(n))
---
+++ Questions/CheckInput.py
@@ -0,0 +1,13 @@
+# Write a Python program to input a number, if it is not a number generates an error message.
+n = input("Enter a input: ")
+sN = int("r")
+sN = n.isdigit()
+if (sN):
+    print("Yes")
+else:
+    print("Sorry it is not a digit")
+
+
+
+
+
