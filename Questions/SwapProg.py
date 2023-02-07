dfc40429353bcee4371f775bbb5f82fc1f31d42c
Sand Man <rajeshbhadra62@gmail.com>
2023-02-07 15:36:11 +0530

    new program is added

---
+++ Questions/SwapProg.py
@@ -0,0 +1,12 @@
+# 40.	 Write a Python program to swap two variables.
+n = input("Enter first value: ")
+n2 = input("Enter second value: ")
+temp = n
+n = n2
+n2 = temp
+
+print("First value is : ")
+print(n)
+print("Second value is : ")
+
+print(n2)
