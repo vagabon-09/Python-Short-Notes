0c47724e383cd5edebe71f16c2175f3e20f68035
Sand Man <rajeshbhadra62@gmail.com>
2023-04-04 01:06:45 +0530

    python dictionary

---
+++ Codes/DIctionary.py
@@ -0,0 +1,6 @@
+dic = {"name": "Rajesh", "roll": "108", "section": "2B"}
+# Dell keyword is used to delete a particular element form a dictionary
+del dic["roll"]
+print("name" in dic)  # it check the key value
+dic["profesion"] = "Student"
+print(dic)
