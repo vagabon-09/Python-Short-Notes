f6bd29af33893b2968a88560c1436fc3dd5cc015
Sand Man <rajeshbhadra62@gmail.com>
2023-06-06 01:09:59 +0530

    New program with dictionary is updated

---
+++ Practice/prog3.py
@@ -0,0 +1,8 @@
+# creating a dictionary in python
+dic1 = {"brand":"Ford","model":"Mustang","year":1964}
+print(len(dic1))
+dic1["year"]=2023
+print(dic1)
+print(dic1["model"])
+dic1.update({'name':"Rajesh"})
+print(dic1)
